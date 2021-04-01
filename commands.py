b8e1a7399b139aa628d1d3d23a35b0dc74bfd165
Asim-Tahir <AsimTahir@protonmail.com>
2021-04-01 12:06:21 +0300

    Commands: Added !flood flood name and !list

---
+++ commands.py
@@ -0,0 +1,86 @@
+from re import template
+from praw.models import Comment
+from logging import getLogger
+from json import loads as json_load
+from os import getenv
+
+from utils import read_file, fill_template
+
+PY_ENV = getenv("PY_ENV", "development")
+
+logger = getLogger("commands")
+floods: dict = json_load(read_file("./floods.json"))
+config: dict = json_load(read_file("./config.json"))
+
+# FOR: Scans by iterating whether there is a match with the comment thrown on all key values
+# `!flood {flood name}` by default
+def flood(comment: Comment) -> None:
+    try:
+        FLOOD_TEMPLATE = fill_template(
+            template=read_file("./template/FLOOD.md"),
+            replaced={
+                "author": comment.author.name,
+                "mark": config["mark"],
+                "command-list": config["commands"]["list"],
+                "source-code": config["source-code"],
+                "flood": "{flood}"
+            },
+        )
+
+        for key, value in floods.items():
+            if (
+                key.lower().strip() in comment.body
+                and f"{config['mark']}{config['commands']['base']}" in comment.body
+                and not comment.saved
+            ):
+                FLOOD_TEMPLATE = fill_template(
+                    template=FLOOD_TEMPLATE, replaced={"flood": value.strip()}
+                )
+                logger.info(
+                    f"{comment.body} from u/{comment.author} in r/{comment.subreddit}"
+                )
+                logger.info(f"Replied to https://reddit.com{comment.permalink}")
+                # DEBUG:
+                if PY_ENV == "production":
+                    comment.reply(FLOOD_TEMPLATE)
+                elif PY_ENV == "development":
+                    logger.info(FLOOD_TEMPLATE)
+    except Exception:
+        comment.unsave()
+        logger.exception(
+            f"Error occurred while replied to https://reddit.com{comment.permalink}"
+        )
+
+
+# FOR: Listing flood `!list` by default
+def listing(comment: Comment) -> None:
+    try:
+        LISTING_TEMPLATE = fill_template(
+            template=read_file("./template/LISTING.md"),
+            replaced={
+                "author": comment.author.name,
+                "floods": "\n".join([f"- {key}" for key in list(floods.keys())]),
+                "mark": config["mark"],
+                "command-base": config["commands"]["base"],
+                "source-code": config["source-code"],
+            },
+        )
+
+        if (
+            f"{config['mark']}{config['commands']['list']}" in comment.body
+            and not comment.saved
+        ):
+            logger.info(
+                f"{comment.body} from u/{comment.author} in r/{comment.subreddit}"
+            )
+            logger.info(f"Replied to https://reddit.com{comment.permalink}")
+            if PY_ENV == "production":
+                comment.reply(LISTING_TEMPLATE)
+                comment.save()
+            elif PY_ENV == "development":
+                logger.info(LISTING_TEMPLATE)
+    except Exception:
+        comment.unsave()
+        logger.exception(
+            f"Error occurred while replied to https://reddit.com{comment.permalink}"
+        )
